a2de8a92bf4990d80cf4abab95f9742f826976c5
joannayhs <joannayuhas@gmail.com>
2019-10-15 19:56:50 -0400

    added helper to ensure on admin could assign roles

--- app/helpers/roles_helper.rb
+++ app/helpers/roles_helper.rb
@@ -1,6 +1,14 @@
 module RolesHelper
 
     def assign_role 
-        render 'roles/assign_role'
+        if current_user.admin? 
+            render 'roles/assign_role'
+        end 
+    end 
+
+    def role_form
+        if current_user.admin? || current_user.roles.include?(self)
+            render  'roles/role_form'
+        end
     end 
 end
